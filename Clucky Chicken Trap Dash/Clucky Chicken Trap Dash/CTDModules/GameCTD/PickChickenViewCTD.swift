77f62cdbd045c60314d5a80b23cf57366182dd59
Dias <dias.atudinov@gmail.com>
2025-03-28 19:18:41 +0500

    pick chicken

--- Clucky Chicken Trap Dash/Clucky Chicken Trap Dash/CTDModules/GameCTD/PickChickenViewCTD.swift
+++ Clucky Chicken Trap Dash/Clucky Chicken Trap Dash/CTDModules/GameCTD/PickChickenViewCTD.swift
@@ -8,8 +8,95 @@
 import SwiftUI
 
 struct PickChickenViewCTD: View {
+    @Environment(\.presentationMode) var presentationMode
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            VStack {
+                Image(.pickChickenTextCTD)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(height: CTDDeviceManager.shared.deviceType == .pad ? 216:108)
+                Spacer()
+                HStack {
+                    
+                    VStack {
+                        Image(.firstChickenCTD)
+                            .resizable()
+                            .scaledToFit()
+                            .frame(height: CTDDeviceManager.shared.deviceType == .pad ? 252:126)
+                        Button {
+                            
+                        } label: {
+                            Image(.goBgCTD)
+                                .resizable()
+                                .scaledToFit()
+                                .frame(height: CTDDeviceManager.shared.deviceType == .pad ? 100:50)
+                        }
+                    }
+                    
+                    
+                    VStack {
+                        Image(.secondChickenCTD)
+                            .resizable()
+                            .scaledToFit()
+                            .frame(height: CTDDeviceManager.shared.deviceType == .pad ? 252:126)
+                        Button {
+                            
+                        } label: {
+                            Image(.goBgCTD)
+                                .resizable()
+                                .scaledToFit()
+                                .frame(height: CTDDeviceManager.shared.deviceType == .pad ? 100:50)
+                        }
+                    }
+                    
+                    
+                    VStack {
+                        Image(.thirdChickenCTD)
+                            .resizable()
+                            .scaledToFit()
+                            .frame(height: CTDDeviceManager.shared.deviceType == .pad ? 252:126)
+                        Button {
+                            
+                        } label: {
+                            Image(.goBgCTD)
+                                .resizable()
+                                .scaledToFit()
+                                .frame(height: CTDDeviceManager.shared.deviceType == .pad ? 100:50)
+                        }
+                    }
+                    
+                }
+                Spacer()
+            }.padding()
+            
+            VStack(spacing: 0) {
+                HStack {
+                    Button {
+                        presentationMode.wrappedValue.dismiss()
+                    } label: {
+                        Image(.backIconCTD)
+                            .resizable()
+                            .scaledToFit()
+                            .frame(height: CTDDeviceManager.shared.deviceType == .pad ? 100:50)
+                    }
+                    
+                    Spacer()
+                }
+                Spacer()
+                
+                
+            }.padding([.leading, .top])
+        }.background(
+            ZStack {
+                Image(.menuBgCTD)
+                    .resizable()
+                    .edgesIgnoringSafeArea(.all)
+                    .scaledToFill()
+            }
+            
+        )
     }
 }
 
