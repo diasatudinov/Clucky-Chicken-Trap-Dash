d266e94aa56f603d53cfa58084d319528b686f0d
Dias <dias.atudinov@gmail.com>
2025-03-31 19:05:19 +0500

    new files

---
+++ Clucky Chicken Trap Dash/Clucky Chicken Trap Dash/CTDModules/CTDMenu/SplashScreenCTD.swift
@@ -0,0 +1,18 @@
+//
+//  SplashScreenCTD.swift
+//  Clucky Chicken Trap Dash
+//
+//  Created by Dias Atudinov on 31.03.2025.
+//
+
+import SwiftUI
+
+struct SplashScreenCTD: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SplashScreenCTD()
+}
